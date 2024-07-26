6cd7f7b64d718a2619b1d3cf8cd5bb99ba9da2f8
reiss timo <t.reiss@eos-ts.com>
2024-07-26 10:14:57 +0200

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # edge-color-changer
+
+How to use:
+
+Microsoft Edge:
+1. Go to edge://extensions/
+2. Enable Developer Mode
+3. Load extension and choose the folder
--- content.js
+++ content.js
@@ -1,49 +1,5 @@
 // content.js
 
-function addHoverEffect(element) {
-    element.addEventListener('mouseenter', () => {
-        element.classList.add('hover-effect');
-    });
-
-    element.addEventListener('mouseleave', () => {
-        element.classList.remove('hover-effect');
-    });
-}
-
-function applyHoverEffects() {
-    const elements = document.querySelectorAll('.kbnTypeahead__item.active');
-    elements.forEach(element => {
-        addHoverEffect(element);
-    });
-}
-
-function throttle(fn, wait) {
-    let time = Date.now();
-    return function() {
-        if ((time + wait - Date.now()) < 0) {
-            fn();
-            time = Date.now();
-        }
-    }
-}
-
-const throttledApplyHoverEffects = throttle(applyHoverEffects, 500);
-
-const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-        if (mutation.type === 'childList' || mutation.type === 'attributes') {
-            throttledApplyHoverEffects();
-        }
-    });
-});
-
-const config = { attributes: true, childList: true, subtree: true };
-
-const targetNodes = document.querySelectorAll('body, .dynamic-content');
-targetNodes.forEach(node => observer.observe(node, config));
-
-applyHoverEffects();
-
 const style = document.createElement('style');
 style.textContent = `
     .hover-effect {
